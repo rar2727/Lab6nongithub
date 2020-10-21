449b2d61efdf4ebcc7058249cd0a620a80e69a1c
rar2727 <rar2727@g.rit.edu>
2020-10-21 15:15:49 -0400

    Initial commit

---
+++ src/rit/cs/AddExpression.java
@@ -0,0 +1,11 @@
+package rit.cs;
+
+public class AddExpression implements Expression {
+    public int evaluate()   {
+        return 0;
+    }
+
+    public String emit()    {
+        return null;
+    }
+}
---
+++ src/rit/cs/Expression.java
@@ -0,0 +1,8 @@
+package rit.cs;
+
+public interface Expression {
+    public int evaluate();
+
+    public String emit();
+
+}
